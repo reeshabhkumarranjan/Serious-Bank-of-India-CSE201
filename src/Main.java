91c3d6acb42de871d6b7f0caffabae1a5e57ea0b
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2018-11-19 18:11:49 +0530

    Updated output format in Main (Removed actual and expected benchmark time). Updated the text-file to match the same.

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        long startTime = System.currentTimeMillis();
         runAverageSimulation();
-        long endTime = System.currentTimeMillis();
-
-        double totalTime = endTime - startTime;
-        double timeInSeconds = totalTime / 1000;
-
-        System.out.println("Time taken for benchmarking: " + timeInSeconds + " seconds");
     }
 
     public static void runAverageSimulation() {
         int accountCount = 10000;
         int iterations = 1;
 
-        System.out.println("\nCalculating average performance. Please wait. (Takes about 20-30 seconds on Intel Core i5 7200U)\n");
+        System.out.println("\nCalculating performance. Please wait.\n");
         averageSimulation(1, accountCount, iterations);
         averageSimulation(2, accountCount, iterations);
         averageSimulation(3, accountCount, iterations);
